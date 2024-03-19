fb472024895caa9e4229eaaf8c5ed604db853627
SolPulido <144694649+SolPulido@users.noreply.github.com>
2024-03-19 12:56:37 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+# Example code
+
+print("Hello World!")
+
+# My Prediction - 
+#The console will print "Hello world!"
+# my prediction was correct
+
+
+##### Task Investigate
+
+# Which part of the example code is the output statement?
+ # Answer: print()
+
+# Which part of the example code is the string?
+ # Answer: "hello world!"
+
+# What would the output of the code print("I love Computing") be?
+ # Answer: i love computing 
+
+# What would happen if the code print("I love Comping") was run?
+ # Answer: The console will distplay i love computing 
+
+# What would happen if the code print("I love Computing" was run?
+ # Answer: There would be an ERROR , because there is not parenthesis 
